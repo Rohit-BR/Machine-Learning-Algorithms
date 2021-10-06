73a82a1ede63503219dafa848af1e6c32b112d69
Rohit-BR <rrohitbalaji@gmail.com>
2021-10-06 19:38:57 +0400

    MLR code

--- Multiple_Linear_Regression.py
+++ Multiple_Linear_Regression.py
@@ -3,11 +3,18 @@ import matplotlib.pyplot as plt
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
+from sklearn.metrics import r2_score
+from sklearn.metrics import mean_squared_error
 
 #Reading dataset and spliting into x and y values
-dataset = pd.read_csv('datasets/Salary_Data.csv')
-x = dataset.iloc[:,:-1].values
-y = dataset.iloc[:,-1].values
+dataset = pd.read_csv('datasets/50_Startups.csv')
+x = dataset.drop('Profit', axis=1)
+y = dataset['Profit']
+
+#Chonverting categorical data into numerical data
+states = pd.get_dummies(x, drop_first=True)
+x = x.drop('State', axis=1)
+x = pd.concat([x,states], axis=1)
 
 #Splitting the dataset into training and testing data
 x_train,x_test,y_train,y_test = train_test_split(x,y,random_state=0,test_size=1/3)
@@ -18,12 +25,8 @@ regressor.fit(x_train,y_train)
 y_train_pred = regressor.predict(x_train)
 y_test_pred = regressor.predict(x_test)
 
-#Plotting Training Data
-plt.scatter(x_train, y_train, color = 'blue')
-plt.plot(x_train, y_train_pred, color = 'red')
-plt.savefig("outputs/LR_Output_1.png")
+print("Test Predictions: ",y_test_pred)
 
-#Plotting Testing Data
-plt.scatter(x_test, y_test, color = 'blue')
-plt.plot(x_test, y_test_pred, color = 'red')
-plt.savefig("outputs/LR_Output_2.png")
+#Predicting the accuracy score
+print("R2 score: ", r2_score(y_test, y_test_pred))
+print("Mean Squared Error: ", mean_squared_error(y_test, y_test_pred))
